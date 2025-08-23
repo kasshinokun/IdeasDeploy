3dc799446b2e5c22ae9ff9ea73c72448ebc13721
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-08-24 03:58:33 +0900

    Update 1a 23-08-2025

--- streamlit_app.py
+++ streamlit_app.py
@@ -1503,8 +1503,8 @@ with col3:
                            min_value=1,
                            max_value=27,
                            step=1)
-    if (go_page > 0 and 
-        go_page < len(sections)-1 
+    if ((go_page-1) > 0 and 
+        (go_page -1)< len(sections)-1 
         and ((go_page-1)!=st.session_state.page_index)):
         if st.button(f"Ir ➡️ para página {go_page}"):
             st.session_state.page_index=go_page-1
